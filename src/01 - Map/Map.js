cd4b792fc444cf8725c474fb8f6e982a09a9b0bf
nathanlschneider <n.l.schneider@gmail.com>
2019-03-10 23:32:48 -0400

    added antd, make tweaks to how the api updates the view

--- src/01 - Map/Map.js
+++ src/01 - Map/Map.js
@@ -2,33 +2,24 @@ import React, { Component } from 'react';
 import './Map.scss';
 import Room from '../Components/Room/Room';
 
-
 export default class Map extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: null
-        }
-    }
-
-    render() {
-        return (
-            <div className="map">
-                <div className="map-content">
-                    <div className="map-content-area">
-                        <Room name="Entryway" id="4"/>
-                        <Room name="Kitchen" id="2"/>
-                        <Room name="DiningRoom" id="3"/>
-                        <Room name="LivingRoom" id="6"/>
-                        <Room name="Hallway" id="5"/>
-                        <Room name="Bathroom" id="1"/>
-                        <Room name="Spare" id="8"/>
-                        <Room name="Master" id="7"/>
-                        <Room name="MasterBath" id="0"/>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div className='map'>
+        <div className='map-content'>
+          <div className='map-content-area'>
+            <Room name='Entryway' id='4' />
+            <Room name='Kitchen' id='2' slider={<input type='range' min='1' max='254' step='22' />} />
+            <Room name='DiningRoom' id='3' slider={<input type='range' min='1' max='254' step='22' />} />
+            <Room name='LivingRoom' id='6' />
+            <Room name='Hallway' id='5' slider={<input type='range' min='1' max='254' step='22' />} />
+            <Room name='Bathroom' id='1' slider={<input type='range' min='1' max='254' step='22' />} />
+            <Room name='Spare' id='8' slider={<input type='range' min='1' max='254' step='22' />} />
+            <Room name='Master' id='7' slider={<input type='range' min='1' max='254' step='22' />} />
+            {/* <Room name='MasterBath' id='0' slider={<input type='range' min='1' max='254' step='22' />} /> */}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- src/Components/Room/Room.js
+++ src/Components/Room/Room.js
@@ -1,49 +1,96 @@
 import React, { Component } from 'react';
+import { Button, Slider } from 'antd';
 import Loader from '../Loader/Loader';
+// import Slider from '../Slider/Slider';
 import './Room.scss';
+import 'antd/dist/antd.css';
+
 const username = 'wQw5uy-TR-jaNQdZblznURuGP43lpCFiJhCq9Um-';
 
 export default class Room extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isOn: null
+      isOn: null,
+      bri: 1,
+      tempBri: 0,
+      disabled: false
     };
     this.handleClick = this.handleClick.bind(this);
     this.turnLightOn = this.turnLightOn.bind(this);
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleAfterChange = this.handleAfterChange.bind(this);
   }
 
   turnLightOn(state) {
-    const data = { on: state };
     fetch(`http://10.0.0.3/api/${username}/groups/${this.props.id}/action`, {
       method: 'PUT',
-      body: JSON.stringify(data)
-    })
-      .then(response => response.text())
-      .then(text => alert(text));
+      body: JSON.stringify({ on: state })
+    });
   }
+
+  handleOnChange(e) {
+    fetch(`http://10.0.0.3/api/${username}/groups/${this.props.id}/action`, {
+      method: 'PUT',
+      body: JSON.stringify({ bri: Math.round(e) })
+    });
+    this.setState({ tempBri: e });
+  }
+
+  handleAfterChange() {
+    this.setState({ bri: this.state.tempBri });
+  }
+
   handleClick() {
     this.state.isOn ? this.turnLightOn(false) : this.turnLightOn(true);
   }
 
-  componentDidMount() {
-    let runner = setInterval(() => {
+  doSetInterval() {
+    this.intervalId = setInterval(() => {
       fetch(`http://10.0.0.3/api/${username}/groups/${this.props.id}`, { mode: 'cors' })
         .then(res => res.text())
-        .then(json => JSON.parse(json))
-        .then(json => this.setState({ isOn: json.state.any_on }))
+        .then(text => JSON.parse(text))
+        .then(json => this.setState({ isOn: json.state.any_on, bri: json.action.bri }))
         .catch(error => console.error('Error:', error));
-    }, 2000);
+    }, 1000);
   }
+  componentDidMount() {
+    fetch(`http://10.0.0.3/api/${username}/groups/${this.props.id}`, { mode: 'cors' })
+      .then(res => res.text())
+      .then(text => JSON.parse(text))
+      .then(json => this.setState({ isOn: json.state.any_on, tempBri: json.action.bri }))
+      .catch(error => console.error('Error:', error));
+    this.doSetInterval();
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
   render() {
     return (
       <>
         <div
           className='room'
-          style={{ gridArea: this.props.name, backgroundColor: this.state.isOn ? 'var(--yellow)' : 'var(--medium)' }}
-          onClick={this.handleClick}
+          style={{
+            gridArea: this.props.name,
+            backgroundColor: this.state.isOn ? 'var(--yellow)' : 'var(--medium)'
+          }}
         >
-          <div className='room-title'>{this.props.name}</div>
+          {this.props.name}
+          <Button className='room-title' onClick={this.handleClick}>
+            {this.state.isOn ? 'Turn Off' : 'Turn On'}
+          </Button>
+          <Slider
+            style={{ width: '100px' }}
+            min={1}
+            max={255}
+            step={254 / 5}
+            value={this.state.tempBri}
+            disabled={!this.state.isOn}
+            onChange={this.handleOnChange}
+            onAfterChange={this.handleAfterChange}
+          />
           <Loader />
         </div>
       </>
